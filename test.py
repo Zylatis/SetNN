a1e5418aa2e94d5fa4dd3f154ce10f331fbfc48c
zylatis <graeme.gossel@gmail.com>
2019-02-04 11:55:41 +1100

    Got image rescaling working for a few test images. Also found git repo of matlab code which automates same task, but doesn't use NN so possibly less generalizable

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import numpy as np
 from matplotlib.image import imread
 from matplotlib import pyplot as plt
-
+import os
+from scipy.misc import imresize
+# from skimage.transform import resize
+from skimage.measure import block_reduce
 
 # Shamelessly pinched but I get it anyway
 def crop_center(img,cropx,cropy):
@@ -12,16 +15,32 @@ def crop_center(img,cropx,cropy):
 
 
 imgs = []
-n_images = 4
-for i in range(n_images):
-	im = imread('imgs/'+str(i)+'.png');
-	im = crop_center(im, 170,100)
-	imgs.append(im)
-	print im.shape
-
-# a = crop_center(a, 170,100)
-# b = crop_center(b, 170,100)
-# c = crop_center(c, 170,100)
+target_x = 100
+target_y = 100
 # d = crop_center(d, 170,100)
-# plt.imshow(b, interpolation='nearest')
-# plt.show()	
+for i in os.listdir("imgs/isolated/"):
+    if i.endswith('.png'):
+    	# print i
+    	im = imread("imgs/isolated/"+str(i))
+    	imgs.append(im)
+    	x,y,z = im.shape
+    	aspect = y/(1.*x)
+
+    	if x > y:
+    		new_size = (100,int(100*aspect))
+    		pad_y = int((100-new_size[1])/2.)
+    		pad_x = 0
+    	else:
+    		new_size =  (int(100/aspect),100)
+    		pad_x = int((100-new_size[0])/2.)
+    		pad_y = 0
+    	print x,y
+    	print pad_x,pad_y
+    	new_im = imresize(im, size = new_size)
+    	new_im = np.pad(new_im, pad_width = ((pad_x,pad_x),(pad_y,pad_y),(0,0)), mode = 'constant', constant_values = (0,0))
+    	# print new_size,pad_x
+    break
+
+
+plt.imshow(new_im, interpolation='nearest')
+plt.show()
