7d9b231cdd2c29207df59f35d0e3e5dcccb6e3d7
Graeme <graeme.gossel@gmail.com>
2019-02-04 16:00:44 +1100

    Working towards augmentation. Can't get imgaug on win machine

---
+++ augment.py
@@ -0,0 +1,19 @@
+from imgaug import augmenters as iaa
+import numpy as np
+from matplotlib.image import imread
+from matplotlib import pyplot as plt
+
+imgs = []
+for i in os.listdir("imgs/processed/"):
+    if i.endswith('.png'):
+        im = imread("imgs/processed/"+str(i))
+        imgs.append(im)
+
+seq = iaa.Sequential([
+    iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
+    iaa.Fliplr(0.5), # horizontally flip 50% of the images
+    iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
+])
+
+
+images_aug = seq.augment_images(imgs)  # done by the library
--- test.py
+++ test.py
@@ -6,41 +6,37 @@ from scipy.misc import imresize
 # from skimage.transform import resize
 from skimage.measure import block_reduce
 
+
+
 # Shamelessly pinched but I get it anyway
 def crop_center(img,cropx,cropy):
-    y,x,c = img.shape
-    startx = x/2 - cropx/2
-    starty = y/2 - cropy/2    
-    return img[ starty:starty + cropy, startx:startx + cropx, :]
+	y,x,c = img.shape
+	startx = x/2 - cropx/2
+	starty = y/2 - cropy/2    
+	return img[ starty:starty + cropy, startx:startx + cropx, :]
 
 
 imgs = []
 target_x = 100
 target_y = 100
-# d = crop_center(d, 170,100)
+
 for i in os.listdir("imgs/isolated/"):
-    if i.endswith('.png'):
-    	# print i
-    	im = imread("imgs/isolated/"+str(i))
-    	imgs.append(im)
-    	x,y,z = im.shape
-    	aspect = y/(1.*x)
+	if i.endswith('.png'):
+		im = imread("imgs/isolated/"+str(i))
+		imgs.append(im)
+		x,y,z = im.shape
+		aspect = y/(1.*x)
 
-    	if x > y:
-    		new_size = (100,int(100*aspect))
-    		pad_y = int((100-new_size[1])/2.)
-    		pad_x = 0
-    	else:
-    		new_size =  (int(100/aspect),100)
-    		pad_x = int((100-new_size[0])/2.)
-    		pad_y = 0
-    	print x,y
-    	print pad_x,pad_y
-    	new_im = imresize(im, size = new_size)
-    	new_im = np.pad(new_im, pad_width = ((pad_x,pad_x),(pad_y,pad_y),(0,0)), mode = 'constant', constant_values = (0,0))
-    	# print new_size,pad_x
-    break
+		if x > y:
+			new_size = (100,int(100*aspect))
+			pad_y = int((100-new_size[1])/2.)
+			pad_x = 0
+		else:
+			new_size =  (int(100/aspect),100)
+			pad_x = int((100-new_size[0])/2.)
+			pad_y = 0
 
-
-plt.imshow(new_im, interpolation='nearest')
-plt.show()
+		new_im = imresize(im, size = new_size)
+		new_im = np.pad(new_im, pad_width = ((pad_x,pad_x),(pad_y,pad_y),(0,0)), mode = 'constant', constant_values = (255,255)) #
+		plt.imshow(new_im, interpolation='nearest')
+		plt.savefig("imgs/processed/proc_img" + str(i) +".png" )
