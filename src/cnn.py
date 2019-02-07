85f69f239df6c09d408e0a0fad2e5e894abaa7d4
zylatis <graeme.gossel@gmail.com>
2019-02-07 12:12:20 +1100

    setup basic tensor layout, still issues regarding dimensionality. Moving towards static tensor def to match other ML code

--- src/cnn.py
+++ src/cnn.py
@@ -13,23 +13,80 @@ from operator import mul
 imgs_folder = "../imgs/aug_imgs/"
 class_map, inverse_class_map = classes.get_labels()
 
-labels = np.loadtxt( "../imgs/labels.dat")
+labels = np.loadtxt( "../imgs/labels.dat").astype(np.int32)
 n_data = len(labels)
 
+n_classes = len(class_map)
 imgs = []
+
+
 for i in range(n_data):
-    im = np.asarray(Image.open( imgs_folder +str(i) + '.png' )).astype(np.int32)
+    im = np.asarray(Image.open( imgs_folder +str(i) + '.png' ))
+    im = tf.convert_to_tensor(im, dtype=tf.float32)
     imgs.append(im)
 
 x_dim, y_dim, n_channels = im.shape
+print im.shape
 # manually check if labels line up 
 # for label in labels:
 	# print inverse_class_map[label]
-print type(imgs)
 
 
-img_train, img_test, class_train, class_test = sk.train_test_split(imgs,labels,test_size=0.10 )
+# img_train, img_test, class_train, class_test = sk.train_test_split(imgs,labels,test_size=0.10 )
 # scaler = StandardScaler().fit(img_train)
-img_train = np.asarray(img_train)
-input_layer = tf.reshape(img_train, [-1, x_dim, y_dim, 3])
-#
+# input_layer = tf.reshape(img_train, [-1, x_dim, y_dim, n_channels])
+
+learning_rate = 0.1
+with tf.name_scope("Input"):
+	inp = tf.placeholder(tf.float32, [None, x_dim,y_dim,n_channels], name = "input")
+
+with tf.name_scope("Output"):
+	out = tf.placeholder(tf.int32, [n_classes], name = "output")
+
+
+with tf.name_scope("layers"):
+	# # Convolutional Layer #1
+	conv1 = tf.layers.conv2d(
+	  inputs=inp,
+	  filters=12,
+	  data_format = 'channels_last',
+	  kernel_size=[5, 5],
+	  padding="same",
+	  activation=tf.nn.relu)
+
+	# # Pooling Layer #1
+	pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[4, 4], strides=4)
+
+	# # Convolutional Layer #2 and Pooling Layer #2
+	conv2 = tf.layers.conv2d(
+	  inputs=pool1,
+	  filters=24,
+	  kernel_size=[5, 5],
+	  padding="same",
+	  data_format = 'channels_last',
+	  activation=tf.nn.relu)
+	pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[4, 4], strides=4)
+	pool2_flat = tf.reshape(pool2, [-1, 16*16*24])
+	dense = tf.layers.dense(inputs=pool2_flat, units=512, activation=tf.nn.relu)
+	dropout = tf.layers.dropout(inputs=dense, rate=0.4)
+	# Logits Layer
+	logits = tf.layers.dense(inputs=dropout, units = n_classes)
+
+
+with tf.name_scope("Cost"):
+	cost = tf.losses.sparse_softmax_cross_entropy(labels=out, logits=logits)
+	
+with tf.name_scope("Train"):
+# add optimizer
+	optimiser = tf.train.AdamOptimizer( learning_rate ).minimize(cost)
+
+# # Dense Layer
+
+print inp
+print conv1
+print pool1
+print conv2
+print pool2
+print pool2_flat
+print dense
+# print logits
--- src/resize.py
+++ src/resize.py
@@ -15,8 +15,8 @@ def crop_center(img,cropx,cropy):
 
 
 imgs = []
-target_x = 250
-target_y = 250
+target_x = 256
+target_y = 256
 count = 0
 labels = []
 for i in os.listdir( imgs_folder + "isolated/"):
@@ -26,19 +26,25 @@ for i in os.listdir( imgs_folder + "isolated/"):
 		imgs.append(im)
 		x,y,z = im.shape
 		aspect = y/(1.*x)
-
+		pad_x = 0
+		pad_y = 0
+		remainder_x = 0
+		remainder_y = 0
 		if x > y:
 			new_size = (target_x,int(target_y*aspect))
 			pad_y = int((target_y-new_size[1])/2.)
-			pad_x = 0
+			remainder_y = target_y - (2*pad_y + new_size[1])
 		else:
 			new_size =  (int(target_x/aspect),target_y)
 			pad_x = int((target_x-new_size[0])/2.)
-			pad_y = 0
+			remainder_x = target_x - (2*pad_x + new_size[0])
+	
 
 		new_im = imresize(im, size = new_size)
-		new_im = np.pad(new_im, pad_width = ((pad_x,pad_x),(pad_y,pad_y),(0,0)), mode = 'constant', constant_values = (255,255)) #
+		new_im = np.pad(new_im, pad_width = ((pad_x + remainder_x,pad_x),(pad_y+remainder_y,pad_y),(0,0)), mode = 'constant', constant_values = (255,255)) #
 		
+
+
 		im = Image.fromarray(new_im)
 		im.save(imgs_folder + "processed/proc_img_ "+ str(i) )
 		count = count + 1
