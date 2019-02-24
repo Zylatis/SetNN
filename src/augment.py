a07d7c6154a81727250edcf49363a2bdfb500468
zylatis <graeme.gossel@gmail.com>
2019-02-24 15:39:20 +1100

    Convert file import loop in augment.py to Python generator

--- src/augment.py
+++ src/augment.py
@@ -10,126 +10,127 @@ import sys
 # e.g. Sometimes(0.5, GaussianBlur(0.3)) would blur roughly every second image.
 sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
+imgs_folder = "../imgs/"
+class_map, inverse_class_map = classes.get_labels()
+
+def yield_files():
+	for i in os.listdir( imgs_folder + "processed/"):
+		if i.endswith('.png'):
+			label =  ("_").join(  (i[:-4]).split('_')[2:6] ).strip()
+			class_val = class_map[label]
+			# classes_seen.append(class_val)
+			im = np.asarray(Image.open( imgs_folder + "processed/"+str(i) )).astype(np.uint8)
+			yield [im, class_val]
+
 # Define our sequence of augmentation steps that will be applied to every image
 # All augmenters with per_channel=0.5 will sample one value _per image_
 # in 50% of all cases. In all other cases they will sample new values
 # _per channel_.
 seq = iaa.Sequential(
-    [
-        # apply the following augmenters to most images
-        iaa.Fliplr(0.5), # horizontally flip 50% of all images
-        iaa.Flipud(0.2), # vertically flip 20% of all images
-        # crop images by -5% to 10% of their height/width
-        sometimes(iaa.CropAndPad(
-            percent=(-0.05, 0.1),
-            pad_mode=ia.ALL,
-            pad_cval=(0, 255)
-        )),
-        sometimes(iaa.Affine(
-            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-            rotate=(-45, 45), # rotate by -45 to +45 degrees
-            shear=(-16, 16), # shear by -16 to +16 degrees
-            order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
-            cval=(0, 255), # if mode is constant, use a cval between 0 and 255
-            mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
-        )),
-        # execute 0 to 5 of the following (less important) augmenters per image
-        # don't execute all of them, as that would often be way too strong
-        iaa.SomeOf((0, 5),
-            [
-                sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
-                iaa.OneOf([
-                    iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
-                    iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
-                    iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
-                ]),
-                iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
-                iaa.Emboss(alpha=(0, 0.5), strength=(0, 0.1)), # emboss images
-                # search either for all edges or for directed edges,
-                # blend the result with the original image using a blobby mask
-                # iaa.SimplexNoiseAlpha(iaa.OneOf([
-                    # iaa.EdgeDetect(alpha=(0.5, 1.0)),
-                    # iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
-                # ])),
-                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=1.), # add gaussian noise to images
-                iaa.OneOf([
-                    iaa.Dropout((0.01, 0.1), per_channel=0.1), # randomly remove up to 10% of the pixels
-                    iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05)),
-                ]),
-                # iaa.Invert(0.05, per_channel=True), # invert color channels
-                iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
-                # iaa.AddToHueAndSaturation((-20, 20)), # change hue and saturation
-                # either change the brightness of the whole image (sometimes
-                # per channel) or change the brightness of subareas
-                # iaa.OneOf([
-                    # iaa.Multiply((0.5, 1.5), per_channel=0.5),
-                    # iaa.FrequencyNoiseAlpha(
-                        # exponent=(-4, 0),
-                        # first=iaa.Multiply((0.5, 1.5), per_channel=True),
-                        # second=iaa.ContrastNormalization((0.5, 2.0))
-                    # )
-                # ]),
-                iaa.ContrastNormalization((0.5, 2.0)), # improve or worsen the contrast
-                # iaa.Grayscale(alpha=(0.0, 1.0)),
-                sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-                # sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
-                sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
-            ],
-            random_order=True
-        )
-    ],
-    random_order=True
+	[
+		# apply the following augmenters to most images
+		iaa.Fliplr(0.5), # horizontally flip 50% of all images
+		iaa.Flipud(0.2), # vertically flip 20% of all images
+		# crop images by -5% to 10% of their height/width
+		sometimes(iaa.CropAndPad(
+			percent=(-0.05, 0.1),
+			pad_mode=ia.ALL,
+			pad_cval=(0, 255)
+		)),
+		sometimes(iaa.Affine(
+			translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
+			rotate=(-45, 45), # rotate by -45 to +45 degrees
+			shear=(-16, 16), # shear by -16 to +16 degrees
+			order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+			cval=(0, 255), # if mode is constant, use a cval between 0 and 255
+			mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+		)),
+		# execute 0 to 5 of the following (less important) augmenters per image
+		# don't execute all of them, as that would often be way too strong
+		iaa.SomeOf((0, 5),
+			[
+				sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
+				iaa.OneOf([
+					iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+					iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
+					iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+				]),
+				iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
+				iaa.Emboss(alpha=(0, 0.5), strength=(0, 0.1)), # emboss images
+				# search either for all edges or for directed edges,
+				# blend the result with the original image using a blobby mask
+				# iaa.SimplexNoiseAlpha(iaa.OneOf([
+					# iaa.EdgeDetect(alpha=(0.5, 1.0)),
+					# iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
+				# ])),
+				iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=1.), # add gaussian noise to images
+				iaa.OneOf([
+					iaa.Dropout((0.01, 0.1), per_channel=0.1), # randomly remove up to 10% of the pixels
+					iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05)),
+				]),
+				# iaa.Invert(0.05, per_channel=True), # invert color channels
+				iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
+				# iaa.AddToHueAndSaturation((-20, 20)), # change hue and saturation
+				# either change the brightness of the whole image (sometimes
+				# per channel) or change the brightness of subareas
+				# iaa.OneOf([
+					# iaa.Multiply((0.5, 1.5), per_channel=0.5),
+					# iaa.FrequencyNoiseAlpha(
+						# exponent=(-4, 0),
+						# first=iaa.Multiply((0.5, 1.5), per_channel=True),
+						# second=iaa.ContrastNormalization((0.5, 2.0))
+					# )
+				# ]),
+				iaa.ContrastNormalization((0.5, 2.0)), # improve or worsen the contrast
+				# iaa.Grayscale(alpha=(0.0, 1.0)),
+				sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+				# sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
+				sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
+			],
+			random_order=True
+		)
+	],
+	random_order=True
 )
 
-imgs_folder = "../imgs/"
-labeled_data = []
-class_map, inverse_class_map = classes.get_labels()
-classes_seen = []
-# can think about best way to represent output: do we make each card unique
-# or fit on subfeatures as a vector output (colour etc)
+n_raw = 0
 for i in os.listdir( imgs_folder + "processed/"):
-    if i.endswith('.png'):
-        label =  ("_").join(  (i[:-4]).split('_')[2:6] ).strip()
-        class_val = class_map[label]
-        classes_seen.append(class_val)
-        im = np.asarray(Image.open( imgs_folder + "processed/"+str(i) )).astype(np.uint8)
-        labeled_data.append([im, class_val])
+	n_raw +=1
+if n_raw == 0:
+	print("Run resize.py first, dumbarse.")
+	exit(0)
 
-if len(labeled_data) == 0:
-    print("Run resize.py first, dumbarse.")
+n_replicates = 3
+count = 0
+img_generator = yield_files()
+all_labels = np.asarray([])
+total = n_raw*n_replicates
+print("Augmenting images and saving")
+for img, label in img_generator:
+	
+	sys.stdout.flush() 
+	replicated_data = np.asarray([ img for i in range(n_replicates)])
+	images_aug = seq.augment_images( replicated_data )
+	all_labels = np.concatenate( (all_labels, [label]*n_replicates))
+	for i in range(n_replicates):
+		im = Image.fromarray(images_aug[i])
+		# im.save( imgs_folder + "aug_imgs/" + str( count) +".png")
+		count += 1
+	perc = int(round(100.*count/(1.*total)))
+	print  "\rProgress: " +str(perc) + "%",
 
 
-exit(0)
-class_seen = classes_seen.sort()
+# print all_labels
+classes_seen = sorted(all_labels)
 classes_seen = list(set(classes_seen))
 n_seen = len(classes_seen)
 restricted_map = {}
 
 for i in range(n_seen):
-    restricted_map[classes_seen[i]] = i
+	restricted_map[int(classes_seen[i])] = i
 
-print("Actual number of classes represented in data: " + str(n_seen) )
+print("\nActual number of classes represented in data: " + str(n_seen) )
+all_labels = map(restricted_map.get,all_labels)
 
-n_replicates = 30
-n_raw = len(labeled_data)
-total = n_replicates*n_raw
-all_labels = np.asarray([])
-count = 0
-
-print("Augmenting images and saving")
-for img in labeled_data:
-    perc = int(round(100*count/(1.*total)))
-    print  "\rProgress: " +str(perc) + "%",
-    sys.stdout.flush() 
-    label = img[1]
-    replicated_data = np.asarray([ img[0] for i in range(n_replicates)])
-    all_labels = np.concatenate( (all_labels, [label]*n_replicates))
-    images_aug = seq.augment_images( replicated_data )  # done by the library
-    for i in range(n_replicates):
-        im = Image.fromarray(images_aug[i])
-        im.save( imgs_folder + "aug_imgs/" + str( count) +".png")
-        count += 1
-
-print("")
 print("Saving labels")
 np.savetxt("../imgs/aug_imgs/aug_labels.dat", all_labels, fmt = "%d")
