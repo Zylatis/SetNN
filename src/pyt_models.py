e96449c6b36be12bc461367b0f02f2873204dc93
zylatis <graeme.gossel@gmail.com>
2020-01-04 16:41:23 +1100

    train/test loop enabled, try on GC

--- src/pyt_models.py
+++ src/pyt_models.py
@@ -8,37 +8,33 @@ import numpy as np
 from PIL import Image # gives better output control than matplotlib
 import sklearn.model_selection as sk
 
-
+torch.set_num_threads(4)
 class MyDataset(Dataset):
-    def __init__(self, data, target, transform=None):
-        self.data = torch.from_numpy(data).float()
-        self.target = torch.from_numpy(target).long()
-        self.transform = transform
-        
-    def __getitem__(self, index):
-        x = self.data[index]
-        y = self.target[index]
-        
-        if self.transform:
-            x = self.transform(x)
-        
-        return x, y
-    
-    def __len__(self):
-        return len(self.data)
-
+	def __init__(self, data, target, transform=None):
+		self.data = torch.from_numpy(data).float()
+		self.target = torch.from_numpy(target).long()
+		self.transform = transform
+		
+	def __getitem__(self, index):
+		x = self.data[index]
+		y = self.target[index]
+		
+		if self.transform:
+			x = self.transform(x)
+		
+		return x, y
+	
+	def __len__(self):
+		return len(self.data)
 
 # Device configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # Hyper parameters
-num_epochs = 50
+num_epochs = 4
 batch_size = 100
 learning_rate = 0.001
 
-
-
-# model = "vec_"
 model= ""
 print("### Setup ###")
 
@@ -48,7 +44,7 @@ vec_labels = np.asarray(np.loadtxt( "../imgs/aug_imgs/aug_vec_labels.dat").astyp
 
 # colour, count, shape, fill
 n_data = len(vec_labels)
-n_data = 10
+n_data = 5000
 print("Loading " + str( n_data ) + " images: "),
 
 imgs = []
@@ -76,15 +72,24 @@ img_test = img_test
 class_train = class_train
 class_test = class_test
 
-
-dataset = MyDataset(img_train, class_train)
-loader = DataLoader(
-    dataset,
-    batch_size=10,
-    shuffle=True,
-    num_workers=2,
-    pin_memory=torch.cuda.is_available()
+train_dataset = MyDataset(img_train, class_train)
+train_loader = DataLoader(
+	train_dataset,
+	batch_size=50,
+	shuffle=True,
+	num_workers=4,
+	pin_memory=torch.cuda.is_available()
 )
+
+test_dataset = MyDataset(img_test, class_test)
+test_loader = DataLoader(
+	test_dataset,
+	batch_size=len(class_test),
+	shuffle=True,
+	num_workers=4,
+	pin_memory=torch.cuda.is_available()
+)
+
 
 # Convolutional neural network (two convolutional layers)
 class ConvNet(nn.Module):
@@ -94,15 +99,12 @@ class ConvNet(nn.Module):
 		self.kernels=kernels
 		self.filters = filters
 		
-		# self.layers = [0,0,0,0]
 		self.pos = {"colour" : 0, "counts" : 1, "fill" : 2, "shape" : 3}
 
 		self.branch("colour")
 		self.branch("counts")
 		self.branch("fill")
 		self.branch("shape")
-
-		# self.myparameters = nn.ParameterList(self.layers[0][0])
 
 	def branch(self, name):
 		layer1 = nn.Sequential(
@@ -118,7 +120,6 @@ class ConvNet(nn.Module):
 			nn.MaxPool2d(kernel_size=2, stride=2))
 
 		fc = nn.Linear(int(self.dims[0]/4*self.dims[1]/4*self.filters[1]), 4) # each branch only gives one class
-
 		# Rather than storing things in lists we assign to the class with the nice add_module fn
 		# so it picks up the parameter sets 
 		self.add_module(f"{name}_layer1", layer1)
@@ -138,36 +139,52 @@ class ConvNet(nn.Module):
 		return output_set
 
 def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+	return sum(p.numel() for p in model.parameters() if p.requires_grad)
 model = ConvNet(im.shape,[5,5],[5,5]).to(device)
-
 
 print(count_parameters(model))
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-
 # Train the model
 for epoch in range(num_epochs):
-	for i, (images, labels) in enumerate(loader):
+	for i, (images, labels) in enumerate(train_loader):
 		images = images.to(device)
 		labels = labels.to(device)
 		
 		# Forward pass
 		outputs = model(images)
 		
-		loss = criterion(outputs[0], labels[:,0]) 
-		# for i in range(1,4):
-			# loss +=  criterion(outputs[i], labels[:,i])  
-		# print(outputs[1])
-		# exit(0)
-		# # Backward and optimize
+		loss = criterion(outputs[0], labels[:,0])
+		for i in range(1,4):
+			loss+= criterion(outputs[i], labels[:,i])
+
+		# Backward and optimize
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
 		
-		# exit(0)
-		print(loss.item())
-		print(outputs[0][0])
-		print(labels[0])
+	
+	print(epoch+1, loss.item())
+
+loaders = {'Train' : train_loader,'Test':test_loader}
+
+for k,loader in loaders.items():
+	with torch.no_grad():
+		correct = np.zeros(4)
+		total = np.zeros(4)
+		for images, labels in loader:
+			images = images.to(device)
+			labels = labels.to(device)
+			outputs = model(images)
+			for i in range(4):
+				_, predicted = torch.max(outputs[i], 1)
+				total[i] += labels.size(0)
+				correct[i] += (predicted == labels[:,i]).sum().item()
+
+		print(f'{k} accuracy:')
+		for i in range(4):
+			print(round(100.*correct[i]/total[i],2))
+
+
